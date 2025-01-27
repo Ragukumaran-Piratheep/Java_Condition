c19a9523a65115a7073a52aef790fb96d7f4af6a
Piratheep <142398970+Ragukumaran-Piratheep@users.noreply.github.com>
2025-01-27 01:07:31 -0800

    Add files via upload

---
+++ CaseExamples.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class CaseExamples
+{
+	public static void main(String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the size: ");
+		int size = sc.nextInt();
+		String output;
+		
+		switch(size)
+		{
+			case 29:
+				output = "Small";
+				break;
+			case 42:
+				output = "Medium";
+				break;
+			case 44:
+				output = "Large";
+				break;
+			case 48:
+				output = "Extra Large";
+				break;
+			default:
+				output = "Unknown";
+				break;
+		}
+		System.out.print("Size: "+output);
+	}
+}
